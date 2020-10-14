3fa53dd7745cc43e54b010cc0dd413e4013db062
Pedro Xavier <pedrohx@yahoo.com.br>
2020-10-14 18:40:25 -0300

    commented cout

--- src/pid_utils.cpp
+++ src/pid_utils.cpp
@@ -26,11 +26,11 @@ uint8_t getColorIndex(uint8_t r, uint8_t g, uint8_t b){
 
 	c_index = b_index + g_index * 6 + r_index * 7 * 6;
 
-	std::cout << 
-		"r index: " << static_cast<uint32_t>(r_index) << std::endl <<
-		"g index: " << static_cast<uint32_t>(g_index) << std::endl <<
-		"b index: " << static_cast<uint32_t>(b_index) << std::endl <<
-		"c index: " << static_cast<uint32_t>(c_index) << std::endl;
+	// std::cout << 
+	// 	"r index: " << static_cast<uint32_t>(r_index) << std::endl <<
+	// 	"g index: " << static_cast<uint32_t>(g_index) << std::endl <<
+	// 	"b index: " << static_cast<uint32_t>(b_index) << std::endl <<
+	// 	"c index: " << static_cast<uint32_t>(c_index) << std::endl;
 
 	return c_index;
 }
