59b91079c97a79f1665a3e3edd67651fc0154a0a
Pedro Xavier <pedrohx@yahoo.com.br>
2020-11-03 02:24:34 -0300

    added argparse

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,34 @@
 #include "pid_utils.hpp"
 #include "bmp.hpp"
 
+#include "argparse/argparse.hpp"
+
 #include <opencv4/opencv2/opencv.hpp>
 
 typedef cv::Vec<uint8_t,3> Vec3b;
 
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-	{
-        std::cout << "usage: " << argv[0] << " <Image_Path>" << std::endl;
-        return 0;
-    }
+int main(int argc, char** argv ){
+
+    argparse::ArgumentParser parser("quantizacao-uniforme");
+
+	parser.add_argument("input")
+		.help("Path to input image.");
+	
+	parser.add_argument("output")
+		.help("Path to output value (default unnamed.bmp)")
+		.default_value("unnamed.bmp");
+
+	try {
+		parser.parse_args(argc,argv);
+	}
+	catch(const std::exception& e) {
+		std::cerr << e.what() << '\n';
+		std::cerr << parser << std::endl;
+		exit(0);
+	}
     
 	cv::Mat input_image, flipped_image;
-    input_image = cv::imread( argv[1], 1 );
+    input_image = cv::imread( parser.get<std::string>("input").c_str(), 1 );
 
 	if ( !input_image.data )
 	{
@@ -57,7 +71,7 @@ int main(int argc, char** argv )
         data_list.pop_front();
     }
 
-	out_bitmap.write("teste_out.bmp");
+	out_bitmap.write(parser.get<std::string>("output").c_str());
 
 	return 0;
 }
